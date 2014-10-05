d5e9bb48fb21419f81402a93be4f6ba1d6a8df88
Joakim Lundin <joakim.lundin@bisnode.com>
2014-10-05 20:50:17 +0200

    Started tweaking the script with warn and alert levels

--- water_level_notifyer/water_level_notifyer.ino
+++ water_level_notifyer/water_level_notifyer.ino
@@ -1,5 +1,5 @@
-#define FLEX_TOO_HI 475
-#define FLEX_TOO_LOW 465
+#define FLEX_LEVEL_WARNING 475
+#define FLEX_LEVEL_ALERT 400
 #define ONBOARD_LED 13
 #define FLEX_SENSOR 0
 
@@ -14,13 +14,24 @@ void setup()
   pinMode(ONBOARD_LED, OUTPUT);
 }
 
+void SendWaterNormal(int bend_value, int bend_state)
+{
+  digitalWrite(ONBOARD_LED, LOW);
+  Serial.print("Water Level Returned To Normal bend_value=");
+  Serial.println(bend_value);
+}
+
+void SendWaterWarning(int bend_value, int bend_state)
+{
+  digitalWrite(ONBOARD_LED, HIGH);
+  Serial.print("Water Level Warning bend_value=");
+  Serial.println(bend_value);
+}
+
 void SendWaterAlert(int bend_value, int bend_state)
 {
-  digitalWrite(ONBOARD_LED, bend_state ? HIGH : LOW);
-  if (bend_state)
-    Serial.print("Water Level Threshold Exceeded, bend_value=");
-  else
-    Serial.print("Water Level Returned To Normal bend_value=");
+  digitalWrite(ONBOARD_LED, HIGH);
+  Serial.print("Water Level Alert bend_value=");
   Serial.println(bend_value);
 }
 
@@ -36,21 +47,32 @@ void loop()
   // limits have been defined
   Serial.print("bend_value=");
   Serial.println(bend_value);
-	
+
   switch (bend_state)
   {
     case 0: // bend_value does not exceed high or low values
-      if (bend_value >= FLEX_TOO_HI || bend_value <= FLEX_TOO_LOW)
+      if (bend_value <= FLEX_LEVEL_WARNING)
       {
         bend_state = 1;
-        SendWaterAlert(bend_value, bend_state);
+        SendWaterWarning(bend_value, bend_state);
       }
       break;
     case 1: // bend_value exceeds high or low values
-      if (bend_value < FLEX_TOO_HI && bend_value > FLEX_TOO_LOW)
+      if (bend_value <= FLEX_LEVEL_ALERT)
+      {
+        bend_state = 2;
+        SendWaterAlert(bend_value, bend_state);
+      }
+      else if (bend_state > FLEX_LEVEL_WARNING)
       {
         bend_state = 0;
-        SendWaterAlert(bend_value, bend_state);
+        SendWaterNormal(bend_value, bend_state);
+      }
+      break;
+    case 2:
+      if (bend_state > FLEX_LEVEL_ALERT) {
+        bend_state = 1;
+        SendWaterWarning(bend_value, bend_state);
       }
       break;
   }
